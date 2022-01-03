a77363115046a47dbb80d44c12748ecbb8f5ad18
Chris <chris_lindstrom@outlook.com>
2022-01-03 18:17:03 +0100

    Setting up router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
   return (
     <div className="App">
-      hej
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   )
 }
