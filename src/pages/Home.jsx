1808734b34b0094f14d2f761167fd9b5882ab8bd
Chris <chris_lindstrom@outlook.com>
2022-01-06 00:23:06 +0100

    styling home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,12 @@ const Home = () => {
     <div className={styles.homeWrapper}>
       <div className={styles.heroWrapper}>
         <img src={heroBG} alt="homepage background"/>
+        <div className={styles.homepageMessage}>
+          {/* <h1>Welcome</h1> */}
+          <h1>Hello Photographers!</h1>
+          <h2>We make it easy for you to create and share albums with your clients</h2>
+          <button className={styles.homepageRegisterBtn}>Get started</button>
+        </div>
       </div>
     </div>
   )
